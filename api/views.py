6df389e1dc06fb630668977e7693291076409fdb
Constantin Guidon <constantin.guidon@gmail.com>
2018-06-05 09:54:51 +0200

    adapt permission granularity

--- api/views.py
+++ api/views.py
@@ -1,44 +1,65 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets, mixins, generics
 
 from api.models import Candidate, Professional, Job
 from api.permissions import IsOwnerOrReadOnly, IsProfessional
 from api.serializers import CandidateSerializer, ProfessionalSerializer, JobSerializer, CandidateListSerializer
 from django.db.models import F
 
+from rest_framework.response import Response
 
-class CandidateViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
-    """
+
+class CandidateList(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    generics.GenericAPIView):
     queryset = Candidate.objects.all()
-    serializer_class = CandidateSerializer
+    serializer_class = CandidateListSerializer
     permission_classes = (IsOwnerOrReadOnly, IsProfessional)
 
-    def retrieve(self, request, *args, **kwargs):
-        # https://docs.djangoproject.com/en/2.0/ref/models/instances/#updating-attributes-based-on-existing-fields
-        candidate = self.get_object()
-        candidate.profile_view_count = F('profile_view_count') + 1
-        candidate.save()
-        return super().retrieve(request, *args, **kwargs)
-
-    def list(self, request, **kwargs):
-        """
-        @Override
-        Args:
-            request: Request object
-            **kwargs: TODO
-
-        Returns:
-            Response: The Candidates list
-        """
+    def get(self, request):
         queryset = Candidate.objects.all()
-        job_name = self.request.query_params.get('job_name', None)
+        job_name = request.query_params.get('job_name', None)
         if job_name is not None:
             queryset = queryset.filter(job__name=job_name)
         serializer = CandidateListSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class CandidateDetail(mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      generics.GenericAPIView):
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Increment the profile views
+        Args:
+            request: Request object
+            *args: TBD
+            **kwargs: TBD
+
+        Returns:
+            Response
+        """
+        instance = self.get_object()
+        # https://docs.djangoproject.com/en/2.0/ref/models/instances/#updating-attributes-based-on-existing-fields
+        instance.profile_view_count = F('profile_view_count') + 1
+        instance.save()
+        return super().retrieve(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
 
 class ProfessionalViewSet(viewsets.ModelViewSet):
