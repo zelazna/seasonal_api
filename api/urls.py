5caf5e9b99beeea6580bc5850a80e89702ad6f19
Constantin Guidon <constantin.guidon@gmail.com>
2018-06-05 09:56:26 +0200

    clean

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,6 @@ from api import views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-# router.register(r'candidates', views.CandidateViewSet)
 router.register(r'professionals', views.ProfessionalViewSet)
 router.register(r'jobs', views.JobViewSet)
 
